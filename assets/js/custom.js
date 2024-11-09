3f6f050dc7a819c8ce984feac63d9ac06eed38a8
amitpaultl <amitpaultl@gmail.com>
2024-11-09 23:19:37 +0600

    add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,24 @@
 (function ($) {
     "use strict";
 
+           // Preloader
+        
+           $(window).on('load', function() {
+            $('#status').fadeOut();
+            $('#preloader').delay(250).fadeOut('slow');
+            $('body').delay(250).css({'overflow':'visible'});
+        });
+
+    $(document).ready(function() {
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 150) {
+               $('.bia-menu-area').addClass('bia-fixed');
+            } else {
+               $('.bia-menu-area').removeClass('bia-fixed');
+            }
+        });
+    });
+
 
 /*------------------------
  slider area start
@@ -9,7 +27,20 @@
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+
+          }
+        },
+
+  
+      ]
     
 });
 
