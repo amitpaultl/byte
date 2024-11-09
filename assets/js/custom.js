6bcd621c8bf8b1cb550c180e4938d8910b030e76
amitpaultl <amitpaultl@gmail.com>
2024-11-09 13:20:52 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+(function ($) {
+    "use strict";
+
+
+/*------------------------
+ slider area start
+---------------------------*/
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+    
+});
+
+/*------------------------
+ slider area end
+---------------------------*/
+
+
+})(jQuery);
